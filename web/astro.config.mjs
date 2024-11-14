cf87f1f1844c56c6d3cc8147f1760033d49228e6
ckdt <ronny@allthis.digital>
2024-11-14 16:50:05 +0100

    added astro dependencies and updated the config to use env file

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
+
+// Deconstruct .env file variables
+const {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+  PUBLIC_SANITY_USE_CDN
+} = loadEnv(process.env.NODE_ENV || "", process.cwd(), "");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sanity(
+    {
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: PUBLIC_SANITY_USE_CDN === "true" ,
+    }
+  )]
+});
